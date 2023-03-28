264d8e027408ba28c60f310d580c831f3c911886
shubhamchilatre <127503620+shubhamchilatre@users.noreply.github.com>
2023-03-28 12:40:17 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "AKIA43HLSVMAUJELUBG2"
+  secret_key = "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"
+}
+
+resource "aws_key_pair" "mykey" {
+  key_name   = "mykey-key"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQD3F6tyPEFEzV0LX3X8BsXdMsQz1x2cEikKDEY0aIj41qgxMCP/iteneqXSIFZBp5vizPvaoIR3Um9xK7PGoW8giupGn+EPuxIA4cDM4vzOqOkiMPhz5XK0whEjkVzTo4+S0puvDZuwIsdiW9mxhJc7tgBNL0cYlWSYVkz4G/fslNfRPW5mYAM49f4fhtxPb5ok4Q2Lg9dPKVHO/Bgeu5woMc7RY0p1ej6D4CKFE6lymSDJpW0YHX/wqE9+cfEauh7xZcG0q9t2ta6F6fmX0agvpFyZo8aFbXeUBr7osSCJNgvavWbM/06niWrOvYX2xwWdhXmXSrbX8ZbabVohBK41 email@example.com"
+}
+
+resource "aws_instance" "webserver" {
+  ami           = "ami-04581fbf744a7d11f"
+  instance_type = "t2.micro"
+  
+  tags = {
+    Name = "Assignment1"
+  }
+}
+
+output "aws_instance_public_ip" {
+description = "public ip address"
+value = aws_instance.webserver.public_ip
+}
+
